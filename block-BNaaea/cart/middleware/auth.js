69ddb71e785340191c4ac04f6e655483b18524eb
vivek <kumarvivek313@gmail.comgit>
2021-09-22 10:40:55 +0530

    A-02

---
+++ block-BNaaea/cart/middleware/auth.js
@@ -0,0 +1,34 @@
+var User = require('../models/user');
+
+module.exports = {
+  loggdInUser: (req, res, next) => {
+    if (req.session && req.session.userId) {
+      next();
+    } else {
+      req.flash('error', 'You must be logged-In to perform this action');
+      res.redirect('/users/login');
+    }
+  },
+  isAdmin: (req, res, next) => {
+    if (req.session && req.session.isAdmin === true) {
+      next();
+    } else {
+      res.redirect('/users');
+    }
+  },
+  userInfo: (req, res, next) => {
+    var userId = req.session && req.session.userId;
+    if (userId) {
+      User.findById(userId, 'fullName email', (err, user) => {
+        if (err) return next(err);
+        req.user = user;
+        res.locals.user = user;
+        next();
+      });
+    } else {
+      req.user = null;
+      res.locals.user = null;
+      next();
+    }
+  },
+};
